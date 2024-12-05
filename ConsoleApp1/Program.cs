872015c6fc44f6a9e7153c02d29dbf1f52d695ba
Manh Nguyen <manh.nguyen@thirdsight.net>
2024-12-05 10:45:35 +0800

    SQL Server Profiler -> SQLQuery.sql

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static string baseFolder = string.Empty;
+
+        private static void Main(string[] args)
+        {
+            AppInitializer();
+            UpdateLogFile($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}: Started.");
+            ExecuteSqlTextAllLines();
+            UpdateLogFile($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}: Finished.");
+        }
+
+        private static void AppInitializer()
+        {
+            baseFolder = GetBaseFolder();
+            string filePath = Path.Combine(baseFolder, "TsWebsite.log");
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, string.Empty, Encoding.UTF8);
+            }
+        }
+
+        private static string GetBaseFolder()
+        {
+            string baseFolder = Environment.CurrentDirectory;
+            return baseFolder;
+        }
+
+        private static void UpdateLogFile(string newLine)
+        {
+            string filePath = Path.Combine(baseFolder, "TsWebsite.log");
+            if (File.Exists(filePath) && !string.IsNullOrWhiteSpace(newLine))
+            {
+                string fileContent = File.ReadAllText(filePath);
+                StringBuilder sb = new StringBuilder(fileContent);
+                sb.AppendLine(newLine);
+                fileContent = sb.ToString();
+                File.WriteAllText(filePath, fileContent);
+            }
+        }
+
+        /// <summary>
+        /// Execute TSQL Without using SMO?
+        /// https://stackoverflow.com/questions/8073170/execute-tsql-without-using-smo
+        /// </summary>
+        private static void ExecuteSqlTextAllLines()
+        {
+            string connectionString = GetSqlServerConnectionString();
+            string sqlFilePath = Path.Combine(baseFolder, "SQLQuery.sql");
+            string sqlStatementBatch = File.ReadAllText(sqlFilePath, Encoding.UTF8);
+
+            //All SQL Statements
+            string[] allSqlStatements = ParseSqlStatementBatch(sqlStatementBatch);
+
+            //SQL Statements does not include "--"
+            string[] sqlStatements = new string[allSqlStatements.Length];
+            for (int i = allSqlStatements.Length - 1; i >= 0; i--)
+            {
+                var sqlStatement = allSqlStatements[i].TrimStart();
+                if (!sqlStatement.StartsWith("--"))
+                {
+                    Debug.WriteLine(sqlStatement);
+                    UpdateLogFile(sqlStatement);
+                    sqlStatements[i] = sqlStatement;
+                }
+            }
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlCommand command = sqlConnection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+                    command.CommandTimeout = GetSqlServerCommandTimeout();
+
+                    foreach (string sqlStatement in sqlStatements)
+                    {
+                        int intRtn = 0;
+                        object objRtn = null;
+                        DbDataReader dataReader = null;
+                        XmlReader xmlReader = null;
+                        if (!string.IsNullOrWhiteSpace(sqlStatement))
+                        {
+                            command.CommandText = sqlStatement;
+#if DEBUG
+                            Debug.WriteLine(sqlStatement);
+#else
+                               intRtn              = command.ExecuteNonQuery();
+                               objRtn           = command.ExecuteScalar();
+                            dataReader = command.ExecuteReader();
+                               xmlReader     = command.ExecuteXmlReader();
+#endif
+                        }
+                        Debug.WriteLine(intRtn);
+                        Debug.WriteLine(objRtn);
+                        Debug.WriteLine(dataReader);
+                        Debug.WriteLine(xmlReader);
+                    }
+                }
+            }
+        }
+
+        private static int GetSqlServerCommandTimeout()
+        {
+            int commandTimeout = 30;
+            return commandTimeout;
+        }
+
+        private static string GetSqlServerConnectionString()
+        {
+            string connectionString = "Data Source=MANH;Integrated Security=True;Initial Catalog=mssql;Connect Timeout=30;Pooling=True;Max Pool Size=1000;";
+            return connectionString;
+        }
+
+        public static string[] ParseSqlStatementBatch(string sqlStatementBatch)
+        {
+            // Split the sql into seperate batches by dividing on the GO statement
+            Regex sqlStatementBatchSplitter = new Regex(@"^\s*GO\s*\r?$", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+            return sqlStatementBatchSplitter.Split(sqlStatementBatch);
+        }
+    }
+}
---
+++ ConsoleApp1/SQLQuery.sql
@@ -0,0 +1,322 @@
+﻿--Login into Modern Portal
+--====================================================================================================
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserFolderReadRights @intTypeID=4,@strUserUid=N'91b516bd-9f74-4374-aa72-04d95e084a49',@intPersonalFeature=0,@intOpenFeature=1,@intClosedFeature=1,@intCollabFeature=1,@intSubFolder=1,@intWkSpcID=0,@strWkSpcIDs=N'',@intSearch=0,@strSessionID=N''
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'91b516bd-9f74-4374-aa72-04d95e084a49'
+go
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+
+
+exec GetUserLoginInfoByUserNameOrEmail @strUserName=N'Admin'
+go
+exec UpdateUserLoginLog @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0',@intTypeID=1
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetCurrSiteConfig 
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserFolderReadRights @intTypeID=4,@strUserUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3',@intPersonalFeature=0,@intOpenFeature=1,@intClosedFeature=1,@intCollabFeature=1,@intSubFolder=1,@intWkSpcID=0,@strWkSpcIDs=N'',@intSearch=0,@strSessionID=N''
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetActiveUserEmailWkSpcByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetCurrSiteConfig 
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetActiveUserEmailWkSpcByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetCurrSiteConfig 
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetActiveUserEmailWkSpcByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetCurrSiteConfig 
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserFolderReadRights @intTypeID=4,@strUserUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3',@intPersonalFeature=0,@intOpenFeature=1,@intClosedFeature=1,@intCollabFeature=1,@intSubFolder=1,@intWkSpcID=0,@strWkSpcIDs=N'',@intSearch=0,@strSessionID=N''
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetActiveUserEmailWkSpcByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+exec GetUserLoginLogByLoginUid @strUserLoginUid=N'640c07c1-e2b2-4cd8-bdd6-f0b8876245b3'
+go
+
+exec GetCurrSiteConfig 
+go
+
+exec GetUserInfoByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+exec GetActiveUserEmailWkSpcByUserUid @strUserUid=N'a5c0272e-d776-4690-a890-979e633f6ae0'
+go
+
+exec GetUserInfoByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
+exec GetActiveUserEmailWkSpcByUserUid @strUserUid=N'4b9c207b-c154-49e7-9a21-f756143ec13b'
+go
